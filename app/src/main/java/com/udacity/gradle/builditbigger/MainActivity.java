a7a3adb17c4622b70279b8ab31a0f911a91470cb
Juan Castillo <jcastillo345@gmail.com>
2016-10-11 13:55:40 -0400

    Step 1: Created a Java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.udacity.gradle.jokes.Joker;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, getString(R.string.joke_toast), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, new Joker().getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 }
---
+++ javajokes/src/main/java/com/udacity/gradle/jokes/Joker.java
@@ -0,0 +1,8 @@
+package com.udacity.gradle.jokes;
+
+public class Joker {
+
+    public String getJoke() {
+        return "This is a totally funny joke!";
+    }
+}

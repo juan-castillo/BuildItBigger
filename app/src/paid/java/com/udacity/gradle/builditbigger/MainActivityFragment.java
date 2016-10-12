bae18e570ffec5065696dc88c9a133fe6a0853dc
Juan Castillo <jcastillo345@gmail.com>
2016-10-12 15:21:41 -0400

    Optional: Add Interstitial Ads between button clicks.

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,10 +1,15 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.udacity.gradle.builditbigger.asynctask.FetchJokeTask;
+import com.udacity.gradle.jokedisplay.JokeActivity;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,6 +21,27 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button getJokeButton = (Button) root.findViewById(R.id.joke_button);
+        getJokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tellJoke();
+            }
+        });
+
+        return root;
+    }
+
+    private void tellJoke() {
+        new FetchJokeTask() {
+            @Override
+            protected void onPostExecute(String joke) {
+                Intent intent = new Intent(getContext(), JokeActivity.class);
+                intent.putExtra(JokeActivity.JOKE_KEY, joke);
+                startActivity(intent);
+            }
+        }.execute();
     }
 }

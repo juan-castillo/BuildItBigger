08bcb31b58f00df682019e8e5d182fd1248feb7c
Juan Castillo <jcastillo345@gmail.com>
2016-10-12 12:03:16 -0400

    Step 4: Added a Functional Test to make sure Async task successfully retrieves a non-empty string.

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -1,13 +1,36 @@
 package com.udacity.gradle.builditbigger;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.udacity.gradle.builditbigger.asynctask.FetchJokeTask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+    @Test
+    public void testJokeNotEmpty() {
+        String joke = null;
+
+        FetchJokeTask fetchJokeTask = new FetchJokeTask();
+        fetchJokeTask.execute();
+        try {
+            joke = fetchJokeTask.get();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        assertNotNull(joke);
+        assertNotEquals("", joke);
     }
 }
